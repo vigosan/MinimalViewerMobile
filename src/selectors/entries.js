621d7b56bc28527b69d1b28ebf5eb6cddcfc2aac
Vicent Gozalbes <vigosan@gmail.com>
2017-04-18 21:12:50 +0200

    Return no viewed entries

--- src/selectors/entries.js
+++ src/selectors/entries.js
@@ -4,12 +4,16 @@ export const getEntriesByViewer = (state, identifier) => {
   if (!identifier) {
     return NO_ENTRIES;
   }
+
   const {
     viewers: { byId: viewersById },
-    entries: { byId: entriesById }
+    entries: { byId: entriesById, viewedIds: entriesViewedIds }
   } = state;
   const { entryIds } = viewersById[identifier];
-  return entryIds.map(entryId => entriesById[entryId]);
+  const notViewedEntryIds = entryIds.filter(entryId => {
+    return !entriesViewedIds.includes(entryId);
+  });
+  return notViewedEntryIds.map(entryId => entriesById[entryId]);
 };
 
 export const isLoadingEntries = state => {
